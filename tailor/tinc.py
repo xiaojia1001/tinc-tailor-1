2d9631179e2e6f63641499368d264e923c7a6789
David Newgas <david@geniedb.com>
2012-10-05 17:02:46 -0700

    Port cloudfabric changes to tinc

--- tailor/tinc.py
+++ tailor/tinc.py
@@ -3,12 +3,13 @@ Created on 26 Sep 2012
 
 @author: david
 '''
-from tailor import *
+from tailor import Tailor, Try, AddRepos, UpdateRepos, Install, Mkdir, PutFile, Command, GetFile, Uninstall, Rmdir, Rm, PutDir
+from os import remove
 from logging import DEBUG, INFO
 from errno import ENOENT
 
 class Tinc(Tailor):
-    def install(self, hostnames=None):
+    def install(self, hostnames=[]):
         actions = [
             Try(AddRepos({'redhat':'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm',
                           'centos':'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm'})),
@@ -23,20 +24,20 @@ class Tinc(Tailor):
             Command("tincd -n {netname} -K4096"),
             GetFile('/etc/tinc/cf/hosts/{hostname}', 'hosts/{hostname}')
         ]
-        if hostnames is None:
+        if len(hostnames) is 0:
             hosts = self.hosts
         else:
             hosts = self.hosts.subset(hostnames)
         [hosts.run_action(action) for action in actions]
         
-    def remove(self, hostnames=None):
+    def remove(self, hostnames=[]):
         actions = [
             Uninstall('{tinc_package}'),
             Try(Rmdir('/etc/tinc/{netname}')),
             Try(Rm('/etc/tinc/nets.boot')),
             Command("! pgrep -f '^tincd -n {netname}' || pkill -9 -f '^tincd -n {netname}'"),
         ]
-        if hostnames is None:
+        if len(hostnames) is 0:
             hosts = self.hosts
         else:
             hosts = self.hosts.subset(hostnames)
@@ -67,10 +68,10 @@ class Tinc(Tailor):
     def setup_argparse(parser):
         subparsers = parser.add_subparsers(title='tinc-command', dest='tinc')
         install_parser = subparsers.add_parser('install', help='install tinc on the given hosts.')
-        install_parser.add_argument('install_hosts', type=str, nargs='+')
+        install_parser.add_argument('install_hosts', type=str, nargs='*')
         remove_parser = subparsers.add_parser('remove', help='remove tinc from the given hosts.')
-        remove_parser.add_argument('remove_hosts', type=str, nargs='+')
-        refresh_parser = subparsers.add_parser('refresh', help='reload tinc configuration on all hosts.')
+        remove_parser.add_argument('remove_hosts', type=str, nargs='*')
+        subparsers.add_parser('refresh', help='reload tinc configuration on all hosts.')
     
     def argparse(self, params):
         self.properties['netname']= params.netname
