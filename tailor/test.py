de28079df0f2b57a23c8d142ae05d22c3444eda6
David Newgas <david@geniedb.com>
2012-10-19 14:21:21 +0100

    tests: setHostDelay removes any existing delay

--- tailor/test.py
+++ tailor/test.py
@@ -340,6 +340,10 @@ class GenieTest(Test):
         if isinstance(hosts, Host):
             hosts = [hosts]
         for host in hosts:
+            if host in self._shaping():
+                self.logger.debug("removing delay from '%s'", host.hostname)
+                host.sync_command(host.interpolate("tc qdisc del dev {netname} root"))
+                self._shaping.discard(host)
             self.logger.debug("Adding %d ms delay to '%s'", delay, host.hostname)
             host.sync_command(host.interpolate("tc qdisc add dev {netname} root netem delay %dms" % delay))
             self._shaping.add(host)
