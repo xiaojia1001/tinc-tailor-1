20eae62242e2860d688ffcda8c5b5e4822ed59b2
David Newgas <david@geniedb.com>
2012-11-14 14:50:52 +0000

    Tests: refactor healing with fixture

--- tests/Healing.py
+++ tests/Healing.py
@@ -9,19 +9,24 @@ try:
 except:
     from unittest2.case import expectedFailure
 
-class SingleRecord(GenieTest):
-    """Test conflict resolution over single record"""
+class HealingFixture(GenieTest):
     def setUp(self):
         if len(self.hosts.hosts) < 2:
             self.skipTest("Insufficient Hosts")
-        super(SingleRecord, self).setUp()
+        super(HealingFixture, self).setUp()
         self.master = self.hosts.hosts[0]
         self.slave = self.hosts.hosts[1]
         self.assertSqlSuccess("DROP TABLE IF EXISTS t1;", self.master, database='test')
+        self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
 
     @expectedFailure
+    def tearDown(self):
+        self.runSql("DROP TABLE t1;", self.master, database='test')
+        super(HealingFixture,self).tearDown()
+
+class SingleRecord(HealingFixture):
+    """Test conflict resolution over single record"""
     def runTest(self):
-        self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
         self.partition(self.master)
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", hosts=self.master, database='test')
         self.unpartition()
@@ -29,22 +34,10 @@ class SingleRecord(GenieTest):
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n1\n", database='test')
 
-    def tearDown(self):
-        self.runSql("DROP TABLE t1;", self.master, database='test')
-        super(SingleRecord,self).tearDown()
 
-class SingleRecordWithPostWrite(GenieTest):
+class SingleRecordWithPostWrite(HealingFixture):
     """Test conflict resolution over single record"""
-    def setUp(self):
-        if len(self.hosts.hosts) < 2:
-            self.skipTest("Insufficient Hosts")
-        super(SingleRecordWithPostWrite, self).setUp()
-        self.master = self.hosts.hosts[0]
-        self.slave = self.hosts.hosts[1]
-        self.assertSqlSuccess("DROP TABLE IF EXISTS t1;", self.master, database='test')
-
     def runTest(self):
-        self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
         self.partition(self.master)
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", hosts=self.master, database='test')
         self.unpartition()
@@ -53,23 +46,10 @@ class SingleRecordWithPostWrite(GenieTest):
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n2\n", database='test')
 
-    def tearDown(self):
-        self.runSql("DROP TABLE t1;", self.master, database='test')
-        super(SingleRecordWithPostWrite,self).tearDown()
-
-class Delete(GenieTest):
+class Delete(HealingFixture):
     """Test conflict resolution over single record"""
-    def setUp(self):
-        if len(self.hosts.hosts) < 2:
-            self.skipTest("Insufficient Hosts")
-        super(Delete, self).setUp()
-        self.master = self.hosts.hosts[0]
-        self.slave = self.hosts.hosts[1]
-        self.assertSqlSuccess("DROP TABLE IF EXISTS t1;", self.master, database='test')
-
     @expectedFailure
     def runTest(self):
-        self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", hosts=self.master, database='test')
         self.partition(self.master)
         self.assertSqlSuccess("DELETE FROM t1 WHERE c1=1;", hosts=self.master, database='test')
@@ -77,10 +57,6 @@ class Delete(GenieTest):
         sleep(20)
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n0\n", database='test')
-
-    def tearDown(self):
-        self.runSql("DROP TABLE t1;", self.master, database='test')
-        super(Delete,self).tearDown()
 
 class TableCreate(GenieTest):
     """Test conflict resolution over single record"""
@@ -105,26 +81,13 @@ class TableCreate(GenieTest):
         self.runSql("DROP TABLE t1;", self.master, database='test')
         super(TableCreate,self).tearDown()
 
-class SingleRecordConflict(GenieTest):
+class SingleRecordConflict(HealingFixture):
     """Test conflict resolution over single record"""
-    def setUp(self):
-        if len(self.hosts.hosts) < 2:
-            self.skipTest("Insufficient Hosts")
-        super(SingleRecordConflict, self).setUp()
-        self.master = self.hosts.hosts[0]
-        self.slave = self.hosts.hosts[1]
-        self.assertSqlSuccess("DROP TABLE IF EXISTS t1;", self.master, database='test')
-
     @expectedFailure
     def runTest(self):
-        self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
         self.partition(self.master)
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", database='test')
         self.unpartition()
         sleep(20)
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n1\n", database='test')
-
-    def tearDown(self):
-        self.runSql("DROP TABLE t1;", self.master, database='test')
-        super(SingleRecordConflict,self).tearDown()
