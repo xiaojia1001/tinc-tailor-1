7e73fc3763d95db7b1e28c87c7d74d7296b430b9
David Newgas <david@geniedb.com>
2012-11-14 14:33:36 +0000

    Tests: add a working healing demo
    GDB-486
    
    Currently healing only works when a write occurs (because writes carry the
    NUC).

--- tests/Healing.py
+++ tests/Healing.py
@@ -25,13 +25,37 @@ class SingleRecord(GenieTest):
         self.partition(self.master)
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", hosts=self.master, database='test')
         self.unpartition()
-        sleep(1)
+        sleep(20)
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n1\n", database='test')
 
     def tearDown(self):
         self.runSql("DROP TABLE t1;", self.master, database='test')
         super(SingleRecord,self).tearDown()
+
+class SingleRecordWithPostWrite(GenieTest):
+    """Test conflict resolution over single record"""
+    def setUp(self):
+        if len(self.hosts.hosts) < 2:
+            self.skipTest("Insufficient Hosts")
+        super(SingleRecordWithPostWrite, self).setUp()
+        self.master = self.hosts.hosts[0]
+        self.slave = self.hosts.hosts[1]
+        self.assertSqlSuccess("DROP TABLE IF EXISTS t1;", self.master, database='test')
+
+    def runTest(self):
+        self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
+        self.partition(self.master)
+        self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", hosts=self.master, database='test')
+        self.unpartition()
+        sleep(20)
+        self.assertSqlSuccess("INSERT INTO t1 VALUES (2, RAND()*100000);", hosts=self.master, database='test')
+        self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
+        self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n2\n", database='test')
+
+    def tearDown(self):
+        self.runSql("DROP TABLE t1;", self.master, database='test')
+        super(SingleRecordWithPostWrite,self).tearDown()
 
 class Delete(GenieTest):
     """Test conflict resolution over single record"""
@@ -50,7 +74,7 @@ class Delete(GenieTest):
         self.partition(self.master)
         self.assertSqlSuccess("DELETE FROM t1 WHERE c1=1;", hosts=self.master, database='test')
         self.unpartition()
-        sleep(1)
+        sleep(20)
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n0\n", database='test')
 
@@ -74,7 +98,7 @@ class TableCreate(GenieTest):
         self.assertSqlSuccess("CREATE TABLE t1 (c1 INT PRIMARY KEY, c2 INT) ENGINE=GenieDB;", hosts=self.master, database='test')
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", hosts=self.master, database='test')
         self.unpartition()
-        sleep(1)
+        sleep(20)
         self.assertSqlSame("SHOW CREATE TABLE t1;", database='test')
 
     def tearDown(self):
@@ -97,7 +121,7 @@ class SingleRecordConflict(GenieTest):
         self.partition(self.master)
         self.assertSqlSuccess("INSERT INTO t1 VALUES (1, RAND()*100000);", database='test')
         self.unpartition()
-        sleep(1)
+        sleep(20)
         self.assertSqlSame("SELECT * FROM t1 ORDER BY c1 DESC;", database='test')
         self.assertSqlEqual("SELECT count(*) AS count FROM t1;", "count\n1\n", database='test')
 
